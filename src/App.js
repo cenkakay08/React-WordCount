c77200b9c61750d381f316d284d392b8588a4119
CNEK <cenk_akay08@hotmail.com>
2021-01-11 21:25:02 +0300

    added some css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import splitter from "./stringSplit";
 import React, { useState } from "react";
+import styled from "styled-components";
 
 const ImportFromFileBodyComponent = () => {
   let fileReader;
@@ -26,19 +27,41 @@ const ImportFromFileBodyComponent = () => {
   };
 
   return (
-    <React.Fragment>
-      <div className="upload-expense">
-        <input
-          type="file"
-          id="file"
-          className="input-file"
-          accept=".txt"
-          onChange={(e) => handleFileChosen(e.target.files[0])}
-        />
-      </div>
-      <div>tane kelime var {stringInfo}</div>
-    </React.Fragment>
+    <WrapperAll>
+      <React.Fragment>
+        <Wrapper>
+          <Title>Word Count!</Title>
+        </Wrapper>
+        <div className="upload-expense">
+          <input
+            type="file"
+            id="file"
+            className="input-file"
+            accept=".txt"
+            onChange={(e) => handleFileChosen(e.target.files[0])}
+          />
+        </div>
+        <div>tane kelime var {stringInfo}</div>
+      </React.Fragment>
+    </WrapperAll>
   );
 };
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #ffd369;
+`;
+const Wrapper = styled.section`
+  background: #393e46;
+`;
+const WrapperAll = styled.section`
+  background: #222831;
+  height: 100vh;
+  width: 100vw;
+  * {
+    margin: 0;
+    padding: 0;
+  }
+`;
 export default ImportFromFileBodyComponent;
