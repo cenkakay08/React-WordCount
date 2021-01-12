837ed2e89614485c90f8abd65c8cc60f00c50cc5
Baran Eryürek <baran.4821@hotmail.com>
2021-01-12 20:35:28 +0300

    right Baby!!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import splitter from "./stringSplit";
-import { Table, Column, Cell } from 'fixed-data-table-2'; // npm install fixed-data-table-2
 import React, { useState, useCallback } from "react";
+import { Table, Column, Cell } from "fixed-data-table-2";
 import styled from "styled-components";
 import { useDropzone } from "react-dropzone";
-import { ScrollView } from "@cantonjs/react-scroll-view";
 
 const WordCountApp = () => {
-  const [wordTable, setWordTable] = useState("");
+  const [textItself, setTextItself] = useState("");
+  const [textInfo, setTextInfo] = useState("");
+  const [wordTable, setWordTable] = useState([]);
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((file) => {
       const reader = new FileReader();
@@ -16,22 +17,27 @@ const WordCountApp = () => {
       reader.onloadend = () => {
         // Do whatever you want with the file contents
         const string = reader.result;
+        setTextItself(string);
         console.log(string);
-        var result = splitter(string, "table");
+        console.log(textItself);
+        var result = splitter(string);
+        setTextInfo(splitter(string,"info"));
+        console.log(result);
         setWordTable(result);
       };
       reader.readAsText(file);
     });
   }, []);
+
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
-  
+
   return (
     <WrapperAll>
       <React.Fragment>
-        <Wrapper>
+        <WrapperTitle>
           <Title>Word Count!</Title>
-        </Wrapper>
-        <Div {...getRootProps()}>
+        </WrapperTitle>
+        <DivInput {...getRootProps()}>
           <input
             {...getInputProps()}
             type="file"
@@ -39,70 +45,114 @@ const WordCountApp = () => {
             className="input-file"
             accept=".txt"
           />
-          <Droptext>
-            Drag 'n' drop some files here, or click to select files
-          </Droptext>
-        </Div>
-        <ScrollView style={{ height: "100vh" }}>
-          <div>
+          <Droptext>Drag-drop the file here, or click to select file!</Droptext>
+        </DivInput>
+        <div>
+          <TextItselfdiv>
+            <p>{textItself}</p>
+          </TextItselfdiv>
+          <Tabblediv>
             <Table
-            rowHeight={50}
-            rowsCount={wordTable.length}
-            headerHeight={50}
-            width={300}
-            height={400}
-          >
-            <Column
-              columnKey="0"
-              header={<Cell>Word</Cell>}
-              width={150}
-              cell={({ rowIndex, columnKey }) => {
-                return <Cell>{wordTable[rowIndex][columnKey]}</Cell>;
-              }}
-            />
-            <Column
-              columnKey="1"
-              header={<Cell>Times</Cell>}
-              width={150}
-              cell={({ rowIndex, columnKey }) => {
-                return <Cell>{wordTable[rowIndex][columnKey]}</Cell>;
-              }}
-            />
-          </Table>
-      </div>
-        </ScrollView>
+              rowHeight={20}
+              rowsCount={wordTable.length}
+              headerHeight={30}
+              width={280}
+              height={450}
+            >
+              <Column
+                columnKey="0"
+                header={<Cell>Word</Cell>}
+                width={100}
+                cell={({ rowIndex, columnKey }) => {
+                  return <Cell>{wordTable[rowIndex][columnKey]}</Cell>;
+                }}
+              />
+              <Column
+                columnKey="1"
+                header={<Cell>Times</Cell>}
+                width={200}
+                cell={({ rowIndex, columnKey }) => {
+                  return <Cell>{wordTable[rowIndex][columnKey]}</Cell>;
+                }}
+              />
+            </Table>
+          </Tabblediv>
+          <TextInfodiv>
+            <p>{textInfo}</p>
+          </TextInfodiv>
+        </div>
       </React.Fragment>
     </WrapperAll>
   );
 };
+const TextItselfdiv = styled.div`
+  height: 55vh;
+  width: 33vw;
+  top: 40vh;
+  overflow: auto;
+  position: absolute;
+  font-family: Arial, sans-serif;
+  border: 1px solid;
+  left: 3vw;
+  box-shadow: 5px 10px 8px 10px #888888;
+`;
 
-const Droptext = styled.p``;
-const Div = styled.div`
+const TextInfodiv = styled.div`
+height: 55vh;
+width: 33vw;
+top: 40vh;
+overflow: auto;
+position: absolute;
+font-family: Arial, sans-serif;
+border: 1px solid;
+right: 3vw;
+box-shadow: 5px 10px 8px 10px #888888;
+`;
+
+const Tabblediv = styled.div`
+  height: 55vh;
+  width: 18vw;
+  top: 40vh;
+  left: 40vw;
+  position: absolute;
+  overflow: hidden;
+  border: 1px solid;
+  box-shadow: 5px 10px 8px 10px #888888;
+`;
+const Droptext = styled.p`
+  text-align: center;
+  color: #0f3057;
+  font-weight: bold;
+  font-size: 4vw;
+  width: 100%;
+  line-height: 20vh;
+`;
+const DivInput = styled.div`
   height: 20vh;
   width: 90vw;
   flex: 1;
-  display: flex;
-  flex-direction: column;
   align-items: center;
-  padding: 20px;
-  border-width: 2px;
-  border-radius: 2px;
+  left: 4vw;
+  border-width: 10px;
+  border-radius: 20px;
   border-style: dashed;
   background-color: #fafafa;
   color: #bdbdbd;
   outline: none;
   transition: border 0.24s ease-in-out;
+  position: relative;
 `;
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 5em;
   text-align: center;
   color: #ffd369;
+  font-weight: bold;
 `;
-const Wrapper = styled.section`
-  background: #393e46;
+const WrapperTitle = styled.section`
+  background: #008891;
 `;
 const WrapperAll = styled.section`
-  background: #222831;
+  background: #e7e7de;
   height: 100vh;
   width: 100vw;
   * {
