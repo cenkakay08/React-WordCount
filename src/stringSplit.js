7c38968b93d32fc4533a2f281382431c67a17d8e
Baran Eryürek <baran.4821@hotmail.com>
2021-01-13 00:06:51 +0300

    added comments to stringSplit

--- src/stringSplit.js
+++ src/stringSplit.js
@@ -1,14 +1,17 @@
 function splitter(str, id) {
-    var counts = {};
-    var keys = [];
-    var wordsLength = 0;
+    var counts = {};//object for word and wuantity values
+    var keys = []; //word array
+    var wordsLength = 0;//Created because we dont use 1 letter words on table
+    var data = []; //2d array for return to the table
     
+    //Making string one sraight line
     const allwords = str.replace(/(\n)/gm,"");
+    //Splitting every word with ignoring punctuation
     const words = allwords.split(/\W+/);
     
-    //Check every word and their number
+    //Check every word and their quantity
     for(var i = 0; i < words.length; i++) {
-        var word = words[i].toLowerCase();
+        var word = words[i].toLowerCase();//Make every word to lowercase for better result
         if(word.length > 1) {
             if(counts[word] === undefined) {
                 counts[word] = 1;
@@ -20,26 +23,27 @@ function splitter(str, id) {
         }
     }
 
-    //Sort keys
+    //Sort keys by compare function
     keys.sort(compare);
 
+    //Sorting function by their quantity value - descending
     function compare(a,b) {
         var countA = counts[a];
         var countB = counts[b];
         return countB - countA;
     }
-    var all = [];
 
-    //Create whole string for words and counts
+    //Push words and there quantity by sort
     for(var k = 0; k < keys.length; k++) {
         var key = keys[k];
-        all.push([key,counts[key]]);
+        data.push([key,counts[key]]);
     }
 
+    //returning string info or array for table
     if(id === "info") {        
         return "Length of all words = " + wordsLength + "\nLength of unique words = " + keys.length;
     } else {       
-        return all;
+        return data;
     }
 
 }
